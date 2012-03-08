6120568a21b1dc114bfc76e92f2e733dfb76a5ab
Ben Gamari <bgamari.foss@gmail.com>
2012-03-08 18:41:06 -0500

    Largely rewrite createTemperatureLookup.py

--- Marlin/createTemperatureLookup.py
+++ Marlin/createTemperatureLookup.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python
-#
-# Creates a C code lookup table for doing ADC to temperature conversion
-# on a microcontroller
-# based on: http://hydraraptor.blogspot.com/2007/10/measuring-temperature-easy-way.html
-"""Thermistor Value Lookup Table Generator
+# -*- coding: utf-8 -*-
 
-Generates lookup to temperature values for use in a microcontroller in C format based on: 
-http://hydraraptor.blogspot.com/2007/10/measuring-temperature-easy-way.html
+""" Thermistor Value Lookup Table Generator
 
-The main use is for Arduino programs that read data from the circuit board described here:
-http://make.rrrf.org/ts-1.0
+Generates lookup to temperature values for use with the Marlin RepRap
+firmware.
 
 Usage: python createTemperatureLookup.py [options]
 
-Options:
-  -h, --help            show this help
-  --r0=...          thermistor rating where # is the ohm rating of the thermistor at t0 (eg: 10K = 10000)
-  --t0=...          thermistor temp rating where # is the temperature in Celsuis to get r0 (from your datasheet)
-  --beta=...            thermistor beta rating. see http://reprap.org/bin/view/Main/MeasuringThermistorBeta
-  --r1=...          R1 rating where # is the ohm rating of R1 (eg: 10K = 10000)
-  --r2=...          R2 rating where # is the ohm rating of R2 (eg: 10K = 10000)
-  --num-temps=...   the number of temperature points to calculate (default: 20)
-  --max-adc=...     the max ADC reading to use.  if you use R1, it limits the top value for the thermistor circuit, and thus the possible range of ADC values
+The quantities below refer to the following circuit,
+
+     Vref ──────┐
+                │
+                R2
+                │
+                ├────────┬──── Vout
+                │        │
+               Rth       R1
+                │        │
+                │        │
+               GND      GND
+
+We assume the common thermistor parameterization of T₀, R₀ and beta where,
+
+  R(T) = R₀ * exp(beta * (1/T - 1/T₀))
+  
 """
 
 from math import *
-import sys
-import getopt
 
 class Thermistor:
     "Class to do the thermistor maths"
@@ -57,71 +58,54 @@ class Thermistor:
         v = self.vs * r / (self.rs + r)     # the voltage at the potential divider
         return round(v / self.vadc * 1024)  # the ADC reading
 
-def main(argv):
-
-    r0 = 10000;
-    t0 = 25;
-    beta = 3947;
-    r1 = 680;
-    r2 = 1600;
-    num_temps = int(20);
+def main():
+    import sys
+    from argparse import ArgumentParser
+    import argparse
+    parser = ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                            description=__doc__)
+    parser.add_argument('--r0', type=float, required=True,
+                        help='Resistance of thermistor at T0 in ohms')
+    parser.add_argument('--t0', type=float, default=25,
+                        help='T0 parameter of thermistor in Celcius')
+    parser.add_argument('--beta', type=float, required=True,
+                        help='Beta parameter of thermistor')
+    parser.add_argument('--r1', type=float, default=None,
+                        help='Value of the optional low-side resistor in ohms')
+    parser.add_argument('--r2', type=float, required=True,
+                        help='Value of the high-side resistor in ohms')
+    parser.add_argument('--num-temps', '-n', type=int, default=20,
+                        help='Number of temperature points to generate')
+    parser.add_argument('--max-adc', type=int, default=1023,
+                        help='Maximum ADC value if R1 is used')
+    parser.add_argument('--name', type=str,
+                        help='Produce output as array with given name')
+    args = parser.parse_args()
     
-    try:
-        opts, args = getopt.getopt(argv, "h", ["help", "r0=", "t0=", "beta=", "r1=", "r2="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-        
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif opt == "--r0":
-            r0 = int(arg)
-        elif opt == "--t0":
-            t0 = int(arg)
-        elif opt == "--beta":
-            beta = int(arg)
-        elif opt == "--r1":
-            r1 = int(arg)
-        elif opt == "--r2":
-            r2 = int(arg)
-
-    if r1:
+    max_adc = args.max_adc
+    if args.r1 is not None:
         max_adc = int(1023 * r1 / (r1 + r2));
-    else:
-        max_adc = 1023
-    increment = int(max_adc/(num_temps-1));
-            
-    t = Thermistor(r0, t0, beta, r1, r2)
+        
+    t = Thermistor(args.r0, args.t0, args.beta, args.r1, args.r2)
 
+    increment = int(max_adc/(args.num_temps-1));
     adcs = range(1, max_adc, increment);
-#   adcs = [1, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110, 130, 150, 190, 220,  250, 300]
-    first = 1
 
     print "// Thermistor lookup table for RepRap Temperature Sensor Boards (http://make.rrrf.org/ts)"
     print "// Made with createTemperatureLookup.py (http://svn.reprap.org/trunk/reprap/firmware/Arduino/utilities/createTemperatureLookup.py)"
-    print "// ./createTemperatureLookup.py --r0=%s --t0=%s --r1=%s --r2=%s --beta=%s --max-adc=%s" % (r0, t0, r1, r2, beta, max_adc)
-    print "// r0: %s" % (r0)
-    print "// t0: %s" % (t0)
-    print "// r1: %s" % (r1)
-    print "// r2: %s" % (r2)
-    print "// beta: %s" % (beta)
-    print "// max adc: %s" % (max_adc)
-    print "#define NUMTEMPS %s" % (len(adcs))
-    print "short temptable[NUMTEMPS][2] = {"
+    print "// ", sys.argv
+    
+    if args.name is not None:
+        print "const short %s[][2] PROGMEM = {" % args.name
 
-    counter = 0
-    for adc in adcs:
-        counter = counter +1
-        if counter == len(adcs):
-            print "   {%s, %s}" % (adc, int(t.temp(adc)))
-        else:
-            print "   {%s, %s}," % (adc, int(t.temp(adc)))
-    print "};"
+    for n,adc in enumerate(adcs):
+        print "   {%4d*OVERSAMPLENR, %4d}%s" % (adc, int(t.temp(adc)), '' if n==len(adcs) else ',')
+            
+    if args.name is not None:
+        print "};"
     
 def usage():
     print __doc__
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
